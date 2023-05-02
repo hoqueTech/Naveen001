2fa5b9cf424b7ab140baf30906ac967d926a5435
azizul <azizul45551@gmail.com>
2023-05-02 11:46:19 -0400

    adding/update/deleting files

---
+++ src/LoginPage/LoginHomePage.java
@@ -0,0 +1,16 @@
+package LoginPage;
+
+public class LoginHomePage {
+
+	public static void main(String[] args) {
+		
+		
+		String userName="Admin";
+		
+		String Pass="1234";
+		
+		System.out.println("Login--succesfully............");
+		System.out.println("Open Login Page......................");
+	}
+
+}
---
+++ src/Testpage/PageTest.java
@@ -0,0 +1,9 @@
+package Testpage;
+
+public class PageTest {
+
+	public static void main(String[] args) {
+		System.out.println("test page ...succesfully");
+	}
+
+}
