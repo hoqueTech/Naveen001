8f2e4d58701770fc98f0a3dcb3d77b2ae4a7850b
azizul <azizul45551@gmail.com>
2023-05-02 13:56:26 -0400

    Updating home page

--- src/Pages/Homepage.java
+++ src/Pages/Homepage.java
@@ -3,6 +3,20 @@ package Pages;
 public class Homepage {
 
 	public static void main(String[] args) {
+		
+		
+		
+		String name="Hoque";
+		int age=35;
+		String place="NY";
+		
+		
+		
+		
+		
+		
+		
+		
 System.out.println("first git ..................1");
 
 	}
