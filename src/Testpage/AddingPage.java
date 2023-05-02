b0ffd859d4481359dab3f6834863458a404c0ffe
azizul <azizul45551@gmail.com>
2023-05-02 13:24:40 -0400

    new updatate

--- src/Testpage/AddingPage.java
+++ src/Testpage/AddingPage.java
@@ -8,7 +8,13 @@ public class AddingPage {
 		int a=20;
 		int b=30;
 		int c=a+b;
+		
 		System.out.println(c);
+		String l_n="Hoq";
+		String f_n="Md";
+	
+		System.out.println(l_n+ " "+ f_n);
+		
 	}
 
 }
