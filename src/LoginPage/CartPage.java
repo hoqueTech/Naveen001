84ffe3f773b6dae4631f450a0e8646780cb72620
azizul <azizul45551@gmail.com>
2023-05-06 13:54:02 -0400

    feature added--cart pages

--- src/LoginPage/CartPage.java
+++ src/LoginPage/CartPage.java
@@ -4,6 +4,7 @@ public class CartPage {
 
 	public static void main(String[] args) {
 System.out.println("-------------Cart Page----------------");
+System.out.println("car feature'''''''''''''''''");
 
 	}
 
