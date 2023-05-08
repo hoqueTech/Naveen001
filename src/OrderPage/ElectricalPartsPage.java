05c5345086b613b5b16a92b23bc85f4d299044af
azizul <azizul45551@gmail.com>
2023-05-07 23:43:00 -0400

    order page----

---
+++ src/OrderPage/ElectricalPartsPage.java
@@ -0,0 +1,12 @@
+package OrderPage;
+
+public class ElectricalPartsPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hi all parts are price very regonable");
+
+	}
+
+}
