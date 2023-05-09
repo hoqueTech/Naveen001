dc3f93603bc0f85eb70fa9b1fad64fae06a8e329
azizul <azizul45551@gmail.com>
2023-05-09 16:23:30 -0400

    added stsh

--- src/LoginPage/Sts.java
+++ src/LoginPage/Sts.java
@@ -4,9 +4,15 @@ public class Sts {
 	
 	
 	public void ad() {
+<<<<<<< Updated upstream
 		String ndev2="cart Dev1";
 		
 		System.out.println(ndev2);
+=======
+		String ndev1="cart Dev1";
+		
+		System.out.println(ndev1);
+>>>>>>> Stashed changes
 	}
 
 }
