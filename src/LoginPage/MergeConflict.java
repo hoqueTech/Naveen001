41c53cee86d326be24d24c03b01febf664effe25
hoqueTech <74996006+hoqueTech@users.noreply.github.com>
2023-05-11 12:22:43 -0400

    Create MergeConflict.java

---
+++ src/LoginPage/MergeConflict.java
@@ -0,0 +1,14 @@
+package LoginPage;
+
+public class MergeConflict {
+
+	public static void main(String[] args) {
+		
+		
+		
+		String name="Ajasg";
+		String jobt="Dev2";
+		System.out.println("This is Dev2 team");
+	}
+
+}
