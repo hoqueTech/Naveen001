bfb59ab7698dcab72b96b20dc502f709b8597538
azizul <azizul45551@gmail.com>
2023-05-11 12:41:51 -0400

    Udater the class and finally merge conflict

--- src/LoginPage/MergeConflict.java
+++ src/LoginPage/MergeConflict.java
@@ -6,17 +6,11 @@ public class MergeConflict {
 		
 		
 		
-<<<<<<< HEAD
-		String name1="Naveen";
-		String jobt1="Dev";
-		System.out.println("This is Dev team");
-		
-		System.out.println("hello--------------------------");
-=======
+
 		String name="Ajasg";
 		String jobt="Dev2";
 		System.out.println("This is Dev2 team");
->>>>>>> ee469edba0158b1ce82aaa8bde8425b7fb72eede
+System.out.println("hi-----------");
 	}
 
 }
