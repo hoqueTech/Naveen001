4a5f0e8c0ddb8ebe8ce019fd93e482454ae27a55
azizul <azizul45551@gmail.com>
2023-05-11 12:04:30 -0400

    added classic page

--- src/LoginPage/Sts.java
+++ src/LoginPage/Sts.java
@@ -4,15 +4,14 @@ public class Sts {
 	
 	
 	public void ad() {
-<<<<<<< Updated upstream
-		String ndev2="cart Dev1";
-		
-		System.out.println(ndev2);
-=======
+
 		String ndev1="cart Dev1";
 		
 		System.out.println(ndev1);
->>>>>>> Stashed changes
+
+		if (true) {
+	System.out.println("stash");
+}
 	}
 
 }
