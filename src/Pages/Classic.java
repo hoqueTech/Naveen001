6f4500fee6710b4c6a8a3d1fbb09e3b1740d6ebe
hoqueTech <74996006+hoqueTech@users.noreply.github.com>
2023-05-11 12:03:38 -0400

    Create Classic.java

---
+++ src/Pages/Classic.java
@@ -0,0 +1,18 @@
+package Pages;
+
+public class Classic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		int x=10;
+		int y=20;
+		int z=a+b;
+		
+		System.out.println(z);
+    
+	}
+
+}
