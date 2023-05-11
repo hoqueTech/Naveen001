ee469edba0158b1ce82aaa8bde8425b7fb72eede
hoqueTech <74996006+hoqueTech@users.noreply.github.com>
2023-05-11 12:32:30 -0400

    Create MConflict.java

---
+++ src/OrderPage/MConflict.java
@@ -0,0 +1,15 @@
+package OrderPage;
+
+public class MConflict {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int n1=100;
+		int m1=10;
+		System.out.println(n/m);
+
+	}
+
+}
