9593284b995a93255d36a812208dd4b552934829
azizul <azizul45551@gmail.com>
2023-05-11 12:12:17 -0400

    update the code after conflict

--- src/Pages/Classic.java
+++ src/Pages/Classic.java
@@ -5,22 +5,14 @@ public class Classic {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		
-<<<<<<< HEAD
-		int a=10;
-		int b=20;
-		int c=a+b;
-		
-		System.out.println(c);
-=======
+
 		int x=10;
 		int y=20;
-		int z=a+b;
+		int z=x+y;
 		
 		System.out.println(z);
-    
->>>>>>> 6f4500fee6710b4c6a8a3d1fbb09e3b1740d6ebe
+    String name="Hoque";
+
 	}
 
 }
