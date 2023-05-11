88ffae5bf2e7814543d95c60d3c2c4d7f45a76b4
azizul <azizul45551@gmail.com>
2023-05-11 12:24:24 -0400

    added the mergeConflicr page

---
+++ src/LoginPage/MergeConflict.java
@@ -0,0 +1,14 @@
+package LoginPage;
+
+public class MergeConflict {
+
+	public static void main(String[] args) {
+		
+		
+		
+		String name="Naveen";
+		String jobt="Dev";
+		System.out.println("This is Dev team");
+	}
+
+}
